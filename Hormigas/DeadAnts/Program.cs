ee99c5274538695cdc3d1b74991514b4dba558c9
EmilMejia <1106101@est.intec.edu.do>
2024-02-15 14:28:42 -0400

    Number of ants alive

--- Hormigas/DeadAnts/Program.cs
+++ Hormigas/DeadAnts/Program.cs
@@ -1,1 +1,32 @@
 ﻿
+
+
+
+Console.WriteLine("Ingrese una cadena de caracteres que represente una estampida de hormigas.");
+Console.WriteLine("Ejemplo: ant...ant..ant.ant.an.ant.t.ant.ant");
+string input = Console.ReadLine();
+
+
+int survivors = CountOccurrences(input, "ant");
+
+
+
+
+
+Console.WriteLine($"Number of ants alive: {survivors}");
+Console.WriteLine($"Number of dead ants");
+
+
+static int CountOccurrences(string str, string substr) 
+{ 
+ int count = 0;
+ int i = 0;
+
+
+    while ((i = str.IndexOf(substr, i)) != -1)
+    { 
+        i = i + substr.Length;
+        count++;
+    }
+    return count;
+}
