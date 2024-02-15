d1053fdd004337faec72f1af26f3c5452626d969
EmilMejia <1106101@est.intec.edu.do>
2024-02-14 23:30:38 -0400

    5 casos de prueba

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -5,9 +5,14 @@ def test_lowest_number_conversion():
     assert decimal_a_romano(1) == "I"
 
 
-def test_lowest_number_conversion():
+def test_high_number_conversion():
     assert decimal_a_romano(1900) == "MCM"
 
 def test_non_numeric_entry():
     assert decimal_a_romano(A) == "Invalid digit"
 
+def test_output_out_of_range():
+    assert decimal_a_romano(2000) == "Invalid digit"
+
+def test_Random_numnber():
+    assert decimal_a_romano(1995) == "MCMXCV"
